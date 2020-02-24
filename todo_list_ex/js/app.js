64e087d07eb0e145fcafd20e266c8267e7c47644
Ewerthon Silva <39033581+EwerthonDev@users.noreply.github.com>
2020-02-24 17:35:44 -0300

    Update app.js

--- todo_list_ex/js/app.js
+++ todo_list_ex/js/app.js
@@ -13,7 +13,7 @@ function renderTodos() {
         var todoList = document.createElement('li');
         var todoText = document.createTextNode(todos);
         var todoListErase = document.createElement('button');
-        var todoListEraseText = document.createTextNode("Excluir");
+        var todoListEraseText = document.createTextNode("Delete");
         todoListErase.setAttribute("id", "erase");
         todoListErase.setAttribute("class", "btn btn-dark btn-sm");
         var pos = todo.indexOf(todos);
@@ -34,7 +34,7 @@ renderTodos();
 function adicionar_todo() {
     var conteudo = inputElement.value;
     if (!conteudo) {
-        alert("Preencha com uma tarefa!");
+        alert("Fill with a task!");
     } else {
         todo.push(conteudo);
         inputElement.value = '';
